62ad64b9bfa3abfc05199e158982fa1bc5448014
António Moreira <antonioclm0507@gmail.com>
2022-11-27 12:35:20 +0000

    deploy on render

--- client/src/utils.js
+++ client/src/utils.js
@@ -1,5 +1,5 @@
-import axios from "axios"
+import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://stories-sharing-app.herokuapp.com"
-})
+  baseURL: "https://stories-sharing-app.onrender.com/api",
+});
--- index.js
+++ index.js
@@ -1,33 +1,31 @@
-const express = require('express');
-const connectDB = require('./db/connect');
-const path = require('path');
-require('dotenv').config();
+const express = require("express");
+const connectDB = require("./db/connect");
+const path = require("path");
+require("dotenv").config();
 const app = express();
-const postsRouter = require('./routes/postsRouter');
-const usersRouter = require('./routes/usersRouter');
+const postsRouter = require("./routes/postsRouter");
+const usersRouter = require("./routes/usersRouter");
 
-app.use(express.json())
+app.use(express.json());
 
-app.use('/', postsRouter)
-app.use('/user', usersRouter)
+app.use("/api/", postsRouter);
+app.use("/api/user", usersRouter);
 
+app.use(express.static(path.join(__dirname, "./client/build")));
 
-app.use(express.static(path.join(__dirname, './client/build')));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "././client/build", "index.html"));
+});
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '././client/build', 'index.html'))
-})
-
-
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URI)
-    app.listen(port, console.log(`server is listening on port ${port}....`))
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`server is listening on port ${port}....`));
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
 
 start();
